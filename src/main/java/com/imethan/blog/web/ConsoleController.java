cbff5579410111e1b37dff17cb38a7d0efc299b5
root <12345678>
2020-12-15 19:28:42 +0800

    modify 调整api接口调用权限，调整blog管理页面元素，调整文件路径

--- src/main/java/com/imethan/blog/web/ConsoleController.java
+++ src/main/java/com/imethan/blog/web/ConsoleController.java
@@ -1,7 +1,10 @@
 package com.imethan.blog.web;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @Create 2019-10-31 16:05
  */
 @Controller
+@Log4j2
 @RequestMapping(value = "console")
 public class ConsoleController {
 
@@ -19,9 +23,16 @@ public class ConsoleController {
         return "/console/console";
     }
 
-    @GetMapping(value = "/publish")
-    public String publish(){
-        return "/console/publish";
+    @GetMapping(value = "/article/input")
+    public String inputArticle(){
+        return "/console/article-input";
+    }
+
+    @GetMapping(value = "/article/modify/{id}")
+    public String modifyArticle(@PathVariable String id, Model model){
+        log.info("modify article id={}",id);
+        model.addAttribute("id",id);
+        return "/console/article-modify";
     }
 
     @GetMapping(value = "/setting")
